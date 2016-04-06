14b1ee3dd505a26653550a745d56bec9ff2446e2
willowcheng <e.wingus@Gmail.com>
2016-04-06 16:47:44 -0400

    Improvement

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function(request, response) {
-  response.send("Hello World!");
+app.get('/', function(req, res) {
+  res.send("<h1>Hello World!</h1>");
 });
 
-app.listen(3000);
+app.listen(3000, function() {
+  console.log("The frontend server is running on port 3000!");
+});
